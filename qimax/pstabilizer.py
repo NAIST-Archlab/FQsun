18e52ea360647b73ba396ee9e00b397f9bf306e7
Vu Tuan Hai <haimeohung@gmail.com>
2025-02-17 10:58:24 +0900

    Add module from qimax

---
+++ qimax/pstabilizer.py
@@ -0,0 +1,95 @@
+import numpy as np
+import polars as pl
+from .instructor import Instructor, weightss_to_lambda
+from .utils import word_to_index, char_to_index, index_to_word, create_word_zj
+class PStabilizer:
+    def __init__(self, j: int, num_qubits: int):
+        """PStabilizer is a Pauli term
+        I encode it as two lists: indices (encoded Pauli words) and lambdas
+
+        Args:
+            j (int): index of stabilizer in the stabilizer group (generator)
+            num_qubits (int)
+        """
+        # Init this stabilizer is Z_j = I \otimes ... \otimes I \otimes Z \otimes I \otimes ... \
+        self.num_qubits = num_qubits
+        self.lambdas = np.array([1])
+        self.indices = np.array([word_to_index(create_word_zj(j, num_qubits))])
+        return
+    def at(self, j: int | str):
+        if type(j) == str:
+            j = word_to_index(j)
+        return self.lambdas[np.where(self.indices == j)[0]]
+    def map(self, ins: Instructor):
+        for j, order in enumerate(ins.orders):
+            k = j // 2
+            if order == 0:
+                self.indices, self.lambdas = map_noncx(self.indices, self.lambdas, ins.LUT, k, self.num_qubits)
+            else:
+                self.indices, self.lambdas = map_cx(self.indices, self.lambdas, ins.xoperators[k], self.num_qubits)
+        return
+    def __str__(self) -> str:
+        text = ""
+        for i, index in enumerate(self.indices[:3]):
+            text += (f"{np.round(self.lambdas[i],2)} * {index_to_word(index, self.num_qubits)} + ")
+        text += (f"... + {np.round(self.lambdas[-1], 2)} * {index_to_word(self.indices[-1], self.num_qubits)}")
+        return text
+    
+
+def map_noncx(indices: np.ndarray, lambdas: np.ndarray, LUT, k: int, num_qubits: int):
+    """Given a list of indices and lambdas, return the indices and lambdas of the non-cx gates.
+    Example:
+        Initial: 1*IZ => indices = [3], lambdas = [1]
+        Another: 2*XZ + 3*YY => indices = [7, 10], lambdas = [2, 3]
+    Args:
+        indices (np.ndarray)
+        lambdas (np.ndarray)
+        num_qubits (int)
+        k: index of the operator
+    Returns:
+        np.ndarray, np.ndarray: mapped indices and lambdas
+    """
+    weightss = []
+    for index in indices:
+        weights = []
+        word = index_to_word(index, num_qubits)
+        for j, char in enumerate(word):
+            i_in_lut = char_to_index(char) - 1
+            if i_in_lut == -1:
+                weights.append([1,0,0,0])
+            else: 
+                weights.append(LUT[k][j][i_in_lut])
+        weightss.append(weights)
+    # Weightss's now a 4-d tensor, 4^n x n x 4
+    weightss = np.array(weightss)
+    # Flattening the weightss into new lambdas, => Can be process effeciently on hardware
+    lambdas = weightss_to_lambda(weightss, lambdas)
+    # For most of the cases, lambdas will be sparse
+    # In the worst case, it will be full dense,
+    # and the below lines would be useless.
+    indices = np.nonzero(lambdas)[0]
+    lambdas = np.array(lambdas)[indices]
+    return indices, lambdas
+
+def map_cx(indices: np.ndarray, lambdas: np.ndarray, cxs: np.ndarray, num_qubits: int):
+    """Mapping multple CX gates on a given indices and lambdas
+
+    Args:
+        indices (np.ndarray): Words after converting to integers
+        lambdas (np.ndarray): Corresponding lambdas
+        cxs (np.ndarray): List of [control, target] pairs from k^th CX-operator
+        num_qubits (int)
+
+    Returns:
+        indices, lambdas
+    """
+    for control, target in cxs:
+        # Access by indices
+        # Example: df = [[0,1,2,3,4], [2,5,-3,4,1]]
+        # I want to request element at [1,2,4], 
+        # polors (pl) read saved df and return [5,-3,1]
+        df = pl.read_csv(f'./qimax/db/{num_qubits}_{control}_{target}_cx.csv')
+        selected_rows = df[indices]['out'].to_numpy()
+        indices = np.abs(selected_rows)
+        lambdas[np.where(selected_rows < 0)[0]] *= -1
+    return indices, lambdas
---
+++ qimax/utils.py
@@ -0,0 +1,162 @@
+import numpy as np
+
+def index_to_word(index: int, num_qubits: int) -> str:
+    """Convert a index to coressponding word
+    Example: index 4, 2 (qubits) -> IX
+    Example: index 255, 4 (qubits) -> ZZZZ
+    Args:
+        index (int): An integer from 0 to 4**num_qubits - 1
+        num_qubits (int)
+
+    Raises:
+        ValueError: index out of bounds
+
+    Returns:
+        str: word
+    """
+    char_map = ['i', 'x', 'y', 'z']
+    word = []
+    if index < 0 or index >= 4**num_qubits:
+        raise ValueError('Index out of bounds. must from 0 to 4**num_qubits - 1')
+    for _ in range(num_qubits):
+        word.append(char_map[index % 4])
+        index //= 4
+    return ''.join(reversed(word))
+
+
+
+def word_to_index(word: str) -> int:
+    """Convert word to corresponding index in array of 4^n
+    Example: IX -> 1*4^1 + 0*4^0 = 4
+    Example: IIII -> 0, ZZZZ = 4^4 - 1 = 255
+    Args:
+        word (str): Pauliword
+
+    Returns:
+        int: index
+    """
+    index = 0
+    for char in word:
+        index = index * 4 + char_to_index(char)
+    return index
+
+def char_to_weight(character: str) -> np.ndarray:
+    """X -> [0, 1, 0, 0] = 0*I + 1*X + 0*Y + 0*Z
+
+    Args:
+        character (str): _description_
+
+    Returns:
+        np.ndarray: _description_
+    """
+    if character == "x":
+        return np.array([0, 1, 0, 0])
+    if character == "y":
+        return np.array([0, 0, 1, 0])
+    if character == "z":
+        return np.array([0, 0, 0, 1])
+    if character == "i":
+        return np.array([1, 0, 0, 0])
+
+
+def char_to_index(character: str) -> int:
+    """I,X,Y,Z -> 0,1,2,3
+
+    Args:
+        character (str): _description_
+
+    Returns:
+        int: _description_
+    """
+    if character == "i":
+        return 0
+    if character == "x":
+        return 1
+    if character == "y":
+        return 2
+    if character == "z":
+        return 3
+
+def generate_pauli_combination(num_qubits: int) -> np.ndarray:
+    """Generater 4^num_qubits Pauli combinations 
+    for a given number of qubits.
+    Args:
+        num_qubits (int): _description_
+
+    Returns:
+        np.ndarray: _description_
+    """
+    combinations = []
+    for i in range(0, 4**num_qubits):
+        combinations.append(index_to_word(i, num_qubits	))
+    return combinations
+
+def create_word_zj(j, num_qubits):
+    if j < 0 or j >= num_qubits:
+        raise ValueError('j out of bounds. must from 0 to num_qubits - 1')
+    return "i" * j + "z" + "i" * (num_qubits - j - 1)
+
+def create_zip_chain(num_operators, num_xoperators, is_cx_first):
+    """Create list 0,1,0,1,...
+    If is_cx_first is True, then 1 is first, else 0 is first
+    Args:
+        n (_type_): _description_
+        m (_type_): _description_
+        is_cx_first (bool): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    result = []
+    while num_operators > 0 or num_xoperators > 0:
+        if is_cx_first:
+            if num_xoperators > 0:
+                result.append(1)
+                num_xoperators -= 1
+            if num_operators > 0:
+                result.append(0)
+                num_operators -= 1
+        else:   
+            if num_operators > 0:
+                result.append(0)
+                num_operators -= 1
+            if num_xoperators > 0:
+                result.append(1)
+                num_xoperators -= 1
+    return result
+
+
+"""
+Constants and functions for PauliComposer and PauliDecomposer classes.
+
+See: https://arxiv.org/abs/2301.00560
+"""
+
+import numpy as np
+from numbers import Real
+
+
+# Definition of some useful constants
+PAULI_LABELS = ['I', 'X', 'Y', 'Z']
+NUM2LABEL = {ind: PAULI_LABELS[ind] for ind in range(len(PAULI_LABELS))}
+BINARY = {'I': '0', 'X': '1', 'Y': '1', 'Z': '0'}
+PAULI = {'I': np.eye(2, dtype=np.uint8),
+         'X': np.array([[0, 1], [1, 0]], dtype=np.uint8),
+         'Y': np.array([[0, -1j], [1j, 0]], dtype=np.complex64),
+         'Z': np.array([[1, 0], [0, -1]], dtype=np.int8)}
+
+
+def nbytes(size: int, n_items: int) -> Real:
+    """Return number of bytes needed for a `n_items`-array of `size` bits."""
+    # Bits/element * number of elements / 8 bits/byte
+    n_bytes = size * n_items / 8
+    return int(n_bytes) if n_bytes.is_integer() else n_bytes
+
+
+def convert_bytes(n_bytes: Real) -> str:
+    """Convert a number of bytes `n_bytes` into a manipulable quantity."""
+    for unit in ['iB', 'kiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB']:
+        if n_bytes < 1024:
+            return '%4.2f %s' % (n_bytes, unit)
+        n_bytes /= 1024
+    return '%4.2f YiB' % (n_bytes)
