cf76293e324c0c76f5360867f2296e4490f62af8
Vũ Tuấn Hải <43202025+vutuanhai237@users.noreply.github.com>
2025-02-19 22:04:19 +0900

    Add FX32 exe time for order

--- time/FX32/Bitstream/load_FX24.sh
+++
@@ -1,4 +0,0 @@
-echo 0 > /sys/class/fpga_manager/fpga0/flags
-cp FX24.bit /lib/firmware/
-echo FX24.bit > /sys/class/fpga_manager/fpga0/firmware
-echo 130000000 > /sys/devices/platform/fclk0/set_rate
--- time/FX32/Bitstream/load_FX32.sh
+++
@@ -1,4 +0,0 @@
-echo 0 > /sys/class/fpga_manager/fpga0/flags
-cp FX32.bit /lib/firmware/
-echo FX32.bit > /sys/class/fpga_manager/fpga0/firmware
-echo 110000000 > /sys/devices/platform/fclk0/set_rate
